1fc45a0fd4b338b1f47b9ada7551403e8b688055
actionchain <actionchain@chainguard.dev>
2026-06-16 17:55:13 +0000

    Update TheMrMilchmann/setup-msvc-dev@v4.0.0

---
+++ docs/changelog/README.md
@@ -0,0 +1,18 @@
+### [Release notes for latest version](latest.md)
+
+### [Release notes for all versions](full.md)
+
+### Version History
+
+* 2025-09-01 [4.0.0](4.0.0.md) (latest)
+* 2025-09-01 [3.0.2](3.0.2.md)
+* 2024-07-04 [3.0.1](3.0.1.md)
+* 2023-09-26 [3.0.0](3.0.0.md)
+* 2023-09-26 [2.0.2](2.0.2.md)
+* 2023-07-04 [2.0.1](2.0.1.md)
+* 2022-07-04 [2.0.0](2.0.0.md)
+* 2022-07-04 [1.0.1](1.0.1.md)
+* 2021-11-03 [1.0.0](1.0.0.md)
+
+
+This action is strictly following [SemVer 2.0.0](https://semver.org/spec/v2.0.0.html).
---
+++ src/constants.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2019 ilammy
+ * Copyright (c) 2021-2024 Leon Linhart
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+export const INPUT_ARCHITECTURE = "arch";
+export const INPUT_VS_PATH = "vs-path";
+export const INPUT_SDK = "sdk";
+export const INPUT_SPECTRE = "spectre";
+export const INPUT_TOOLSET = "toolset";
+export const INPUT_UWP = "uwp";
+
+export const INPUT_EXPORT_VCVARSALL = "export-path-to-vcvarsall";
+export const INPUT_EXPORT_VS = "export-path-to-vs";
